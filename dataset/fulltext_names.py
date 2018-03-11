ca22b12c7660da1365e6a67ce49639b763df86da
Avik Pal <avikpal@iitk.ac.in>
2018-03-11 11:05:59 +0530

    Removed unneccesary spaces from _words.txt files

--- dataset/fulltext_names.py
+++ dataset/fulltext_names.py
@@ -1,7 +1,7 @@
 import os
-files = filter(lambda x: x.endswith('.txt'), os.listdir('sentences'))
+files = filter(lambda x: x.endswith('_words.txt'), os.listdir('sentences'))
 
 
-out = open("names"+".txt", "w")
+out = open("names_words"+".txt", "w+")
 for f in files:
 	out.write(f+" ")
---
+++ dataset/sentences/Remove Spaces in Words.py
@@ -0,0 +1,26 @@
+def remWord_Space(file_name):
+    file = open(file_name,'r')
+    a = file.read()
+    b = ''
+    count = 0
+    for i in a:
+        if(i!='\n'):
+            count = 0
+            b += i
+            continue
+        if(count==0):
+            b += i
+            if(i=='\n'):
+                count = 1
+    file.close()
+    file = open(file_name,'w')
+    file.write(b)
+    file.close()
+    print(file_name,end=" ")
+    print("file has been processed")
+
+names = open("names_words.txt","r")
+names_files = names.read().split()
+print("YES")
+for nm in names_files:
+    remWord_Space(nm)
