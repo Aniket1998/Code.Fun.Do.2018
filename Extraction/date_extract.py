b8692ae16d7c78208ce99cc19f7f29877d28f806
avik-pal <avikpal@iitk.ac.in>
2018-04-01 00:41:06 +0530

    Modified regex to meet current needs

--- Extraction/date_extract.py
+++ Extraction/date_extract.py
@@ -5,7 +5,7 @@ import os
 filename = sys.argv[1]
 file = open(filename,"r")
 
-regex = r"[^\.]+\d{1,2}\s\D{3,8}\s\d{2,4}\."
+regex = r"([A-Z][^\.!?]*)(\d{1,2}[t][h]\s\D{3,8}[,]\s\d{2,4}|\d{0,1}[1][s][t]\s\D{3,8}[,]\s\d{2,4}|\d{0,1}[2][n][d]\s\D{3,8}[,]\s\d{2,4}|\d{0,1}[3][r][d]\s\D{3,8}[,]\s\d{2,4}|\d{1,2}\s\D{3,8}[,]\s\d{2,4})\s([a-z][^\.!?]*)([\.!?])"
 
 test_str = file.read()
 
